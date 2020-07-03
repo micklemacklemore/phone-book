140a51ce29d6a993effc24fa65951f60fd579335
Michael Mason <michaelmasontd@gmail.com>
2020-07-03 14:09:23 +1000

    update api doc

--- phonebook/supported_filetypes/filetypes_abstract.py
+++ phonebook/supported_filetypes/filetypes_abstract.py
@@ -1,6 +1,6 @@
 """
     filetypes_abstract.py
-    ------------
+    ---------------------
     This module contains the abstract class which all subclasses from :meth:`phonebook.supported_filetypes.filetypes`
     When adding support for additional storage formats, you must inherit from this class and use it as a template.
 """
@@ -8,26 +8,32 @@
 import abc
 
 class PhoneBookABC(object):
+
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractproperty
     def filetype(self):
-        """
-        :return: File extension of phonebook format
+        """ *string* Returns file extension of phonebook format e.g. 'json'
         """
 
     @abc.abstractmethod
     def write_to(self, file_output, phone_book):
         """
-        Required method for writing the phonebook dictionary to a specified format
-        :param file_output: (file object) file to write to
-        :param phone_book: (dict) phone book
-        :return: (str) string in specified format
+        Method for writing the phonebook dictionary to the specified filetype format
+
+        **file_output** takes a file object to write to
+
+        **phone_book** (dict) phone book
+
+        **returns:** (str) string in specified format
         """
     @abc.abstractmethod
     def read_from(self, file_input):
         """
-        Required method for reading a file to be turned into a dictionary that pBook can read
-        :param file_input: pass an open file
-        :return: (list) python dictionary in phoneBook format
+        Method that takes a file object to read from and creates a dictionary that
+        PhoneBookActions can accept
+
+        **file_input** takes a file object to read from
+
+        **return:** (list) python dictionary in phoneBook format
         """
